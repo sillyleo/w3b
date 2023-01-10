78e2affcbbe6adb74b33d193b6ba875c33e3c78f
Leo Lin <sayaww@gmail.com>
2023-01-10 16:05:23 +0800

    f

---
+++ docs/pages/reactpage$.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Button } from "@sayaww/w3b";
+
+function reactpage$() {
+  return (
+    <div>
+      reactpage$
+      <br />
+      <Button>oisfhjuikls</Button>
+    </div>
+  );
+}
+
+export default reactpage$;
