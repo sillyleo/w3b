9486aefd821f569f66f3efbbd9b470fc20f775dd
Leo Lin <sayaww@gmail.com>
2023-01-10 15:37:40 +0800

    up

--- src/Button/index.tsx
+++ src/Button/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import s from './style.module.css'
+import React from "react";
+import s from "./style.module.css";
 
 interface Props
   extends React.DetailedHTMLProps<
@@ -11,9 +11,9 @@ const Button = ({ className, ...props }: Props) => {
   return (
     <button
       {...props}
-      className={[s.button, className].filter(Boolean).join(' ')}
+      className={[s.button, className].filter(Boolean).join(" ")}
     />
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
