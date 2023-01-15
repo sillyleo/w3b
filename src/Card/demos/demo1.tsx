aea4f77b3f7002632a92b0107b3e8b34d904c38d
Leo Lin <sayaww@gmail.com>
2023-01-15 13:06:08 +0800

    static

--- src/Card/demos/demo1.tsx
+++ src/Card/demos/demo1.tsx
@@ -10,9 +10,7 @@ const Demo1 = () => {
   return (
     <div>
       <Card>stitches card demo</Card>
-      <Card color="white">stitches card demo</Card>
-      <Card>stitches card demo</Card>
-      <Card>stitches card demo</Card>
+      <Card tone="gold">stitches card demo</Card>
     </div>
   );
 };
--- src/Card/index.tsx
+++ src/Card/index.tsx
@@ -1,7 +1,8 @@
 import figmaTokens from "../../tokens/theme.json";
-import { styled } from "../stitches.config";
+import { styled, theme } from "../stitches.config";
 import type * as Stitches from "@stitches/react";
 
+// Method 1
 const colorsArray = Object.keys(figmaTokens.colors);
 
 const colorVariants = colorsArray.reduce((acc, color) => {
@@ -15,24 +16,78 @@ const colorVariants = colorsArray.reduce((acc, color) => {
 
 console.log(colorVariants);
 
+// Method 2
+
+// const COLOR_PATTERN = "$––color––";
+// const REGEX = /\"([^\"]*?\$––color––[^\"]*?)\"/;
+
+// export const generateColorPaletteVariants = (css: Stitches.CSS) => {
+//   const cssString = JSON.stringify(css);
+
+//   return Object.values(theme.colors).reduce((prev, color) => {
+//     const substringToReplace = REGEX.exec(cssString);
+//     if (!substringToReplace?.length) throw Error("No Color Token provided.");
+
+//     const replacedSubstring = substringToReplace[0].replace(
+//       COLOR_PATTERN,
+//       `$${color.token}`
+//     );
+
+//     const replacedString = cssString.replace(
+//       substringToReplace[0],
+//       replacedSubstring
+//     );
+
+//     return { ...prev, [color.token]: JSON.parse(replacedString) };
+//   }, {}) as Record<keyof typeof theme.colors, Stitches.CSS>;
+// };
+
+// const colorVariants = generateColorPaletteVariants({
+//   backgroundColor: "$––color––",
+// });
+
+// console.log(colorVariants);
+
 const Card = styled("div", {
   borderRadius: "9999px",
   color: "$white",
   fontSize: "13px",
   padding: "10px 15px",
   variants: {
-    color: {
-      black: {
-        backgroundColor: "$black",
-      },
-      white: {
-        backgroundColor: "$white",
-      },
-      ...colorVariants,
+    tone: {
+      // all color keys from theme.colors
+      tomato: { backgroundColor: "$tomato6" },
+      red: { backgroundColor: "$red6" },
+      crimson: { backgroundColor: "$crimson6" },
+      pink: { backgroundColor: "$pink6" },
+      plum: { backgroundColor: "$plum6" },
+      purple: { backgroundColor: "$purple6" },
+      violet: { backgroundColor: "$violet6" },
+      indigo: { backgroundColor: "$indigo6" },
+      blue: { backgroundColor: "$blue6" },
+      cyan: { backgroundColor: "$cyan6" },
+      teal: { backgroundColor: "$teal6" },
+      green: { backgroundColor: "$green6" },
+      grass: { backgroundColor: "$grass6" },
+      brown: { backgroundColor: "$brown6" },
+      orange: { backgroundColor: "$orange6" },
+      sky: { backgroundColor: "$sky6" },
+      mint: { backgroundColor: "$mint6" },
+      lime: { backgroundColor: "$lime6" },
+      yellow: { backgroundColor: "$yellow6" },
+      amber: { backgroundColor: "$amber6" },
+      gold: { backgroundColor: "$gold6" },
+      bronze: { backgroundColor: "$bronze6" },
+      gray: { backgroundColor: "$gray6" },
+      mauve: { backgroundColor: "$mauve6" },
+      slate: { backgroundColor: "$slate6" },
+      sage: { backgroundColor: "$sage6" },
+      olive: { backgroundColor: "$olive6" },
+      sand: { backgroundColor: "$sand6" },
     },
   },
   defaultVariants: {
-    color: "white",
+    tone: "violet",
   },
 });
 
