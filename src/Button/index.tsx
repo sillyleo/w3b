3b0eaebcb4730fedf0a6bc7bf6a0e958b75d1dc1
Leo Lin <sayaww@gmail.com>
2023-01-20 11:00:57 +0800

    add basic props

--- src/Button/index.tsx
+++ src/Button/index.tsx
@@ -1,8 +1,8 @@
-import { style, styleVariants } from '@macaron-css/core';
-import { cva, VariantProps } from 'class-variance-authority';
-import React from 'react';
-import { theme } from '../theme';
-import figmaTokens from '../theme.json';
+import { style, styleVariants } from "@macaron-css/core";
+import { cva, VariantProps } from "class-variance-authority";
+import React from "react";
+import { theme } from "../theme";
+import figmaTokens from "../theme.json";
 
 //  all colors as allTones from figmaTokens.colors
 // eg. {blue: "blue", red: "red", green: "green"}
@@ -24,71 +24,71 @@ const baseClass = style({
   // letterSpacing: theme.letterSpacing.increased,
   // lineHeight: theme.lineHeight.heading,
   padding: theme.spacing[8],
-  ':hover': {
-    backgroundColor: 'lightgray'
-  }
+  ":hover": {
+    backgroundColor: "lightgray",
+  },
 });
 
 // Sizes
 
 const sizeClass = styleVariants({
   sm: {
-    padding: theme.spacing[2]
+    padding: theme.spacing[2],
   },
   md: {
-    padding: theme.spacing[3]
+    padding: theme.spacing[3],
   },
   lg: {
-    padding: theme.spacing[5]
-  }
+    padding: theme.spacing[5],
+  },
 });
 
 //  Dynamic tones and intents
 
 const primaryClass = styleVariants(allTones, (tone) => ({
   backgroundColor: theme.colors[tone][9],
-  ':hover': {
-    backgroundColor: theme.colors[tone][10]
+  ":hover": {
+    backgroundColor: theme.colors[tone][10],
   },
-  color: theme.colors.white
+  color: theme.colors.white,
 }));
 
 const secondaryClass = styleVariants(allTones, (tone) => ({
   backgroundColor: theme.colors[tone][8],
-  ':hover': {
-    backgroundColor: theme.colors[tone][9]
+  ":hover": {
+    backgroundColor: theme.colors[tone][9],
   },
-  color: theme.colors.white
+  color: theme.colors.white,
 }));
 
 const tertiaryClass = styleVariants(allTones, (tone) => ({
   backgroundColor: theme.colors[tone][3],
-  ':hover': {
-    backgroundColor: theme.colors[tone][4]
+  ":hover": {
+    backgroundColor: theme.colors[tone][4],
   },
-  color: theme.colors.black
+  color: theme.colors.black,
 }));
 
 const transparentClass = styleVariants(allTones, (tone) => ({
-  background: 'transparent',
-  ':hover': {
-    backgroundColor: theme.colors[tone][2]
+  background: "transparent",
+  ":hover": {
+    backgroundColor: theme.colors[tone][2],
   },
   // border: "none",
-  color: theme.colors[tone][10]
+  color: theme.colors[tone][10],
 }));
 
 // Compose variants with class names
 
 function getVariant(intent, tone) {
   switch (intent) {
-    case 'primary':
+    case "primary":
       return primaryClass[tone];
-    case 'secondary':
+    case "secondary":
       return secondaryClass[tone];
-    case 'tertiary':
+    case "tertiary":
       return tertiaryClass[tone];
-    case 'transparent':
+    case "transparent":
       return transparentClass[tone];
     default:
       return primaryClass[tone];
@@ -100,32 +100,35 @@ const buttonStyle = cva(baseClass, {
     size: {
       sm: sizeClass.sm,
       md: sizeClass.md,
-      lg: sizeClass.lg
-    }
+      lg: sizeClass.lg,
+    },
   },
   defaultVariants: {
-    size: 'lg'
-  }
+    size: "lg",
+  },
 });
 
-type ToneType = keyof Colors
+type ToneType = keyof Colors;
 
 // Button component
 export interface ButtonProp extends VariantProps<typeof buttonStyle> {
   children?: React.ReactNode;
-  intent?: 'primary' | 'secondary' | 'tertiary' | 'transparent';
+  /**
+   * the intent of button
+   * @defaultValue 'primary'
+   */
+  intent?: "primary" | "secondary" | "tertiary" | "transparent";
+
   // TODO: doesn't work...
   tone?: ToneType;
 }
 
-
 const Button = ({ size, intent, tone, ...props }: ButtonProp) => {
-
   return (
     <button
       className={buttonStyle({
         size,
-        className: getVariant(intent, tone) // for dynamic variants that's not composed with variants API
+        className: getVariant(intent, tone), // for dynamic variants that's not composed with variants API
       })}
       {...props}
     />
