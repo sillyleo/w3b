9c21068620a088e3cc4f17a0a47a5813b281446f
Leo Lin <sayaww@gmail.com>
2023-01-27 17:36:57 +0800

    custom icon children

--- src/IconButton/README.md
+++ src/IconButton/README.md
@@ -9,4 +9,12 @@ This is a **markdown** document of the `IconButton` component.
 
 <Demo src="./demos/demo1.tsx" />
 
+To use custom image, simple wrap your icon with `<IconButton>` as children.
+
+```tsx
+<IconButton>
+  <img src="https://img.alicdn.com/tfs/TB1ZQY9X4D1gK0jSZFsXXbldVXa-200-200.png" />
+</IconButton>
+```
+
 <TsInfo src="./index.tsx" name="IconButtonProps" />
--- src/IconButton/index.tsx
+++ src/IconButton/index.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { iconButtonStyle, IconButtonVariants } from "./style.css";
-import { clsx } from "clsx";
+import React from 'react';
+import { iconButtonStyle, IconButtonVariants } from './style.css';
+import { clsx } from 'clsx';
 import {
   primaryClass,
   secondaryClass,
@@ -13,17 +13,17 @@ import {
   lgSecondaryShadowClass,
   smGhostShadowClass,
   mdGhostShadowClass,
-  lgGhostShadowClass,
-} from "../styles/theme.css";
-import Icon from "../Icon";
+  lgGhostShadowClass
+} from '../styles/theme.css';
+import Icon from '../Icon';
 
 // set variant class names
 export function getVariant(intent: string, tone: keyof Colors) {
-  if (intent === "primary") {
+  if (intent === 'primary') {
     return primaryClass[tone];
-  } else if (intent === "secondary") {
+  } else if (intent === 'secondary') {
     return secondaryClass[tone];
-  } else if (intent === "ghost") {
+  } else if (intent === 'ghost') {
     return ghostClass[tone];
   } else {
     return primaryClass[tone];
@@ -37,30 +37,30 @@ export function getShadowVariant(
   tone: keyof Colors,
   intent: string
 ) {
-  if (shadow && intent === "primary") {
-    if (size === "sm") {
+  if (shadow && intent === 'primary') {
+    if (size === 'sm') {
       return smPrimaryShadowClass[tone];
-    } else if (size === "md") {
+    } else if (size === 'md') {
       // console.log(mdPrimaryShadowClass[tone]);
       return mdPrimaryShadowClass[tone];
     } else {
       return lgPrimaryShadowClass[tone];
     }
   }
-  if (shadow && intent === "secondary") {
-    if (size === "sm") {
+  if (shadow && intent === 'secondary') {
+    if (size === 'sm') {
       return smSecondaryShadowClass[tone];
-    } else if (size === "md") {
+    } else if (size === 'md') {
       return mdSecondaryShadowClass[tone];
     } else {
       return lgSecondaryShadowClass[tone];
     }
   }
 
-  if (shadow && intent === "ghost") {
-    if (size === "sm") {
+  if (shadow && intent === 'ghost') {
+    if (size === 'sm') {
       return smGhostShadowClass[tone];
-    } else if (size === "md") {
+    } else if (size === 'md') {
       return mdGhostShadowClass[tone];
     } else {
       return lgGhostShadowClass[tone];
@@ -71,10 +71,10 @@ export function getShadowVariant(
 export interface IconButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     IconButtonVariants {
-  size?: "sm" | "md" | "lg";
-  align?: "left" | "center" | "right" | "between" | "around";
+  size?: 'sm' | 'md' | 'lg';
+  align?: 'left' | 'center' | 'right' | 'between' | 'around';
   tone?: keyof Colors;
-  intent?: "primary" | "secondary" | "ghost";
+  intent?: 'primary' | 'secondary' | 'ghost';
   leftIcon?: React.ReactNode;
   rightIcon?: React.ReactNode;
   gradient?: boolean;
@@ -86,22 +86,22 @@ export interface IconButtonProps
 }
 
 export const IconButton = ({
-  // list all available props here and default values
-  size = "md",
-  align = "center",
-  tone = "sand",
-  intent = "primary",
-  children,
-  leftIcon,
-  rightIcon,
-  gradient = false,
-  shadow = false,
-  name = "camera",
-  color,
-  strokeWidth,
-  fill,
-  ...props
-}: IconButtonProps) => {
+                             // list all available props here and default values
+                             size = 'md',
+                             align = 'center',
+                             tone = 'sand',
+                             intent = 'primary',
+                             children,
+                             leftIcon,
+                             rightIcon,
+                             gradient = false,
+                             shadow = false,
+                             name = 'camera',
+                             color,
+                             strokeWidth,
+                             fill,
+                             ...props
+                           }: IconButtonProps) => {
   return (
     <button
       // clsx is only for combing multiple classes together
@@ -109,7 +109,7 @@ export const IconButton = ({
         iconButtonStyle({
           size: size,
           align: align,
-          gradient: gradient,
+          gradient: gradient
         }),
         getVariant(intent, tone),
         getShadowVariant(shadow, size, tone, intent)
@@ -117,7 +117,11 @@ export const IconButton = ({
       {...props}
     >
       {leftIcon}
-      <Icon name="camera" color={color} strokeWidth={strokeWidth} fill={fill} />
+      {children ? (
+        children
+      ) : (
+        <Icon name={name} color={color} strokeWidth={strokeWidth} fill={fill} />
+      )}
       {rightIcon}
     </button>
   );
