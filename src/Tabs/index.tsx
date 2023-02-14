810fbdd6daee589f789ec30ba70277ae5cd74831
Leo Lin <sayaww@gmail.com>
2023-02-14 20:55:54 +0800

    add style-able tab components

--- src/Tabs/index.tsx
+++ src/Tabs/index.tsx
@@ -1,27 +1,34 @@
-import * as Tabs from '@radix-ui/react-tabs';
+import * as Tabs from "@radix-ui/react-tabs";
+import Bento from "src/Bento";
+import "./style.css";
+import { contentStyle, listStyle, rootStyle, triggerStyle } from "./style.css";
 
-const Root = (props) => {
-  return (
-    <Tabs.Root {...props} />
-  )
-}
+export type RootProps = React.ComponentProps<typeof Tabs.Root> &
+  React.ComponentProps<typeof Bento> & {};
+
+const Root = (props: RootProps) => {
+  return <Bento as={Tabs.Root} className={rootStyle} {...props} />;
+};
+
+export type ListProps = React.ComponentProps<typeof Tabs.List> &
+  React.ComponentProps<typeof Bento>;
 
 const List = (props) => {
-  return (
-    <Tabs.List {...props} />
-  )
-}
+  return <Bento as={Tabs.List} className={listStyle} {...props} />;
+};
+
+export type TriggerProps = React.ComponentProps<typeof Tabs.Trigger> &
+  React.ComponentProps<typeof Bento>;
 
 const Trigger = (props) => {
-  return (
-    <Tabs.Trigger {...props} />
-  )
-}
+  return <Bento as={Tabs.Trigger} className={triggerStyle} {...props} />;
+};
+
+export type ContentProps = React.ComponentProps<typeof Tabs.Content> &
+  React.ComponentProps<typeof Bento>;
 
 const Content = (props) => {
-  return (
-    <Tabs.Content {...props} />
-  )
-}
+  return <Bento as={Tabs.Content} className={contentStyle} {...props} />;
+};
 
-export default { Root, List, Trigger, Content }
+export default { Root, List, Trigger, Content };
--- src/Tabs/style.css.ts
+++ src/Tabs/style.css.ts
@@ -2,7 +2,14 @@ import { style, styleVariants } from "@vanilla-extract/css";
 import { allTones } from "src/constants";
 import { theme } from "src/styles/theme.css";
 
-export const base = style({
-  display: "block",
-  alignSelf: "stretch",
+export const rootStyle = style({
+});
+
+export const listStyle = style({
+});
+
+export const triggerStyle = style({
+});
+
+export const contentStyle = style({
 });
