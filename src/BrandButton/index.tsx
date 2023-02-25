96574b38028433bd3928629571e5a5520298a961
Leo Lin <sayaww@gmail.com>
2023-02-25 11:48:53 +0800

    need to separate brand button and normal buttons

--- src/BrandButton/index.tsx
+++ src/BrandButton/index.tsx
@@ -1,152 +1,173 @@
-import {css, styled} from "src/stitches.config";
+import { css, styled } from "src/stitches.config";
 import Spinner from "../Spinner";
-
-{
-    /* <div
-    style={{
-      display: "flex",
-      alignItems: "center",
-      padding: 8,
-      borderRadius: 9999,
-      overflow: "hidden",
-      backgroundColor: "#3ceaaa",
-    }}
-  >
-    <div
-      style={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        padding: 2,
-      }}
-    >
-
-    </div>
-    <div
-      style={{
-        padding: 4,
-        fontWeight: 500,
-        fontSize: 14,
-        textAlign: "center",
-        lineHeight: 150,
-      }}
-    >
-      Button
-    </div>
-  </div>; */
-}
-
+import _ from "lodash";
 const StyledButton = styled("button", {
-    display: "flex",
-    alignItems: "center",
-    fontWeight: "$medium",
-    overflow: "hidden",
-    lineHeight: 1.5,
-    transition: '$fast',
-    '&:active': {
-        transform: "scale(0.98) translateY(1px)",
-    },
-    '&:disabled': {
-        transform: 'none',
-        cursor: "not-allowed",
-    },
-    variants: {
-        size: {
-            // xs, s,m,l
-            xs: {
-                px: "$2",
-                py: "$2",
-                fontSize: "$sm",
-                "& .spinner": {
-                    boxSize: 16,
-                },
+  display: "flex",
+  alignItems: "center",
+  fontWeight: "$medium",
+  overflow: "hidden",
+  lineHeight: 1.5,
+  transition: "$fast",
+  "&:active": {
+    transform: "scale(0.98) translateY(1px)",
+  },
+  "&:disabled": {
+    transform: "none",
+    cursor: "not-allowed",
+    opacity: 0.75,
+  },
+  borderRadius: "$xl",
+  variants: {
+    size: {
+      xs: {
+        borderRadius: "$lg",
 
-            },
-            s: {
-                px: "$3",
-                py: "$2",
-                "& .spinner": {
-                    boxSize: 20,
-                },
-
-            },
-            m: {
-                px: "$4",
-                py: "$3",
-                "& .spinner": {
-                    boxSize: 20,
-                },
-            },
-            l: {
-                px: "$6",
-                py: "$4",
-                "& .spinner": {
-                    boxSize: 24,
-                },
-            }
+        px: "$2",
+        py: "$2",
+        fontSize: "$sm",
+        "& .spinner": {
+          boxSize: 16,
         },
-        intent: {
-            primary: {
-                borderRadius: "$full",
+      },
+      s: {
+        borderRadius: "$xl",
 
-                background: "$colors$primary",
-                color: 'black',
-                '&:hover': {
-                    background: "$colors$hover",
-                },
-                '&:disabled': {
-                    opacity: 0.5,
-                },
-            },
-            secondary: {
-                borderRadius: "$xl",
+        px: "$3",
+        py: "$2",
+        "& .spinner": {
+          boxSize: 20,
+        },
+      },
+      m: {
+        borderRadius: "$xl",
 
-                background: "$colors$gray6",
-                color: "$colors$gray12",
-                '&:hover': {
-                    background: "$colors$gray7",
-                },
-                '&:disabled': {
-                    opacity: 0.5,
-                },
-            }
+        px: "$4",
+        py: "$3",
+        "& .spinner": {
+          boxSize: 20,
+        },
+      },
+      l: {
+        borderRadius: "$2xl",
 
-        }
-
+        px: "$6",
+        py: "$4",
+        "& .spinner": {
+          boxSize: 24,
+        },
+      },
     },
-    defaultVariants: {
-        size: "m",
-        intent: "primary"
-    }
-
+    brand: {
+      primary: {
+        borderRadius: "$full",
+        background: "$colors$primary",
+        color: "black",
+        "&:hover": {
+          background: "$colors$hover",
+        },
+      },
+      secondary: {
+        borderRadius: "$xl",
+        background: "$colors$gray6",
+        color: "$colors$gray12",
+        "&:hover": {
+          background: "$colors$gray7",
+        },
+      },
+    },
+    // border: {
+    //   1: {
+    //     borderRadius: "$lg",
+    //   },
+    //   2: {
+    //     borderRadius: "$xl",
+    //   },
+    //   3: {
+    //     borderRadius: "$full",
+    //   },
+    // },
+  },
+  defaultVariants: {
+    size: "m",
+  },
+  compoundVariants: [{}],
 });
 
 const innterWrapper = css({
-    px: 6
-})
+  px: 6,
+  "& > p": {
+    lineHeight: "inherit",
+  },
+});
 
+function getButtonToneStyle(
+  tone: keyof Colors
+  //   intent: "primary" | "secondary" | "ghost"
+) {
+  if (!tone) {
+    return;
+  } else if (
+    tone === "sky" ||
+    tone === "mint" ||
+    tone === "lime" ||
+    tone === "yellow" ||
+    tone === "amber"
+  ) {
+    return {
+      bgTone9: tone,
+      "&:hover": {
+        bgTone10: tone,
+      },
+      "&:active": {
+        bgTone11: tone,
+      },
+      color: "black",
+    };
+  } else if (
+    tone === "gray" ||
+    tone === "mauve" ||
+    tone === "slate" ||
+    tone === "sage" ||
+    tone === "olive" ||
+    tone === "sand"
+  ) {
+    // grayscale series
+    return {
+      bgTone12: tone,
+      "&:hover": {
+        bgTone12: tone,
+      },
+      "&:active": {
+        bgTone11: tone,
+      },
+      color: `$${tone}2`,
+    };
+  } else {
+    return {
+      bgTone9: tone,
+      "&:hover": {
+        bgTone10: tone,
+      },
+      "&:active": {
+        bgTone11: tone,
+      },
+      color: "white",
+    };
+  }
+}
 
-function BrandButton(props) {
+export interface BrandButtonProps
+  extends React.ComponentProps<typeof StyledButton> {
+  tone?: keyof Colors;
+}
 
-    function getToneStyle() {
-        if (!props.tone) {
-            return props.css
-        }
-        return {
-            bgTone10: props.tone,
-            ...props.css
-        }
-    }
-
-    return (
-        <StyledButton
-
-            css={getToneStyle()}
-            {...props}>
-            <Spinner className={"spinner"}/>
-            <span className={innterWrapper()}>{props.children}</span>
-        </StyledButton>
-    );
+function BrandButton({ tone = "slate", ...props }: BrandButtonProps) {
+  return (
+    <StyledButton css={_.merge(getButtonToneStyle(tone), props.css)} {...props}>
+      <Spinner className={"spinner"} />
+      <span className={innterWrapper()}>{props.children}</span>
+    </StyledButton>
+  );
 }
 
 export default BrandButton;
--- src/Spinner/index.tsx
+++ src/Spinner/index.tsx
@@ -1,22 +1,29 @@
 export default function Spinner(props) {
-    return (
+  return (
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <animateTransform
+        attributeName="transform"
+        type="rotate"
+        from="0 0 0"
+        to="360 0 0"
+        begin="0s"
+        dur="1s"
+        repeatCount="indefinite"
+      />
 
-        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"
-
-             {...props}
-        >
-            <animateTransform attributeName="transform"
-                              type="rotate"
-                              from="0 0 0" to="360 0 0"
-                              begin="0s" dur="1s"
-                              repeatCount="indefinite"
-            />
-
-            <path fill-rule="evenodd" clip-rule="evenodd"
-                  d="M12.6667 8C12.6667 5.42267 10.5773 3.33333 8 3.33333V2C11.3137 2 14 4.68629 14 8C14 11.3137 11.3137 14 8 14C4.68629 14 2 11.3137 2 8H3.33333C3.33333 10.5773 5.42267 12.6667 8 12.6667C10.5773 12.6667 12.6667 10.5773 12.6667 8Z"
-                  fill="currentColor"/>
-        </svg>
-
-
-    );
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M12.6667 8C12.6667 5.42267 10.5773 3.33333 8 3.33333V2C11.3137 2 14 4.68629 14 8C14 11.3137 11.3137 14 8 14C4.68629 14 2 11.3137 2 8H3.33333C3.33333 10.5773 5.42267 12.6667 8 12.6667C10.5773 12.6667 12.6667 10.5773 12.6667 8Z"
+        fill="currentColor"
+      />
+    </svg>
+  );
 }
