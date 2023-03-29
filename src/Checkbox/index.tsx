e93790b53304c32cb2d216ee2d0ef005b053312e
Leo Lin <sayaww@gmail.com>
2023-03-29 11:59:49 +0800

    shipped

--- src/Checkbox/index.tsx
+++ src/Checkbox/index.tsx
@@ -1,125 +1,13 @@
 import React from "react";
-import {
-  Checkbox as CheckboxPrimitive,
-  CheckboxProps,
-  useCheckboxState,
-} from "reakit/Checkbox";
-import { clsx } from "clsx";
-import { Bento, LucideIcon } from "../../src";
-import { formTone } from "../styles/theme.css";
-import { TextProps } from "../Text";
-import {
-  checkboxEmoji,
-  checkboxIcon,
-  checkboxIndicator,
-  checkboxLabel,
-  checkboxRoot,
-} from "./style.css";
-import { VisuallyHidden } from "react-aria";
-
-export interface CheckboxExtendedProps extends CheckboxProps {
-  //realkit
-  disabled?: boolean | undefined;
-  //custom
-  defaultChecked?: boolean | undefined;
-  children?: React.ReactNode;
-  tone?: keyof Colors;
-  fontSize?:
-    | "xs"
-    | "sm"
-    | "base"
-    | "lg"
-    | "xl"
-    | "2xl"
-    | "3xl"
-    | "4xl"
-    | "5xl"
-    | "6xl";
-  checkEmoji?: React.ReactNode;
-}
-
-function Checkbox(
-  {
-    disabled,
-    defaultChecked,
-    children,
-    checkEmoji,
-    fontSize,
-    tone = "gray",
-    ...props
-  }: CheckboxExtendedProps,
-  ref: React.ForwardedRef<HTMLInputElement>
-) {
-  const checkbox = useCheckboxState({
-    state: defaultChecked,
-  });
-  // console.log(checkbox.state);
-  return (
-    <Bento
-      fontSize={fontSize}
-      as={"label"}
-      className={checkboxLabel[!!disabled ? "disabled" : "enabled"]}
-    >
-      <VisuallyHidden>
-        <CheckboxPrimitive
-          disabled={disabled}
-          ref={ref}
-          {...checkbox}
-          {...props}
-        />
-      </VisuallyHidden>
-      <div
-        data-state={
-          props.checked !== undefined
-            ? props.checked
-              ? "checked"
-              : "unchecked"
-            : checkbox.state
-            ? "checked"
-            : "unchecked"
-        } // styling based on state
-        //@ts-ignore
-        className={clsx(checkboxRoot, formTone[tone])}
-      >
-        {/* use checked if it's present, else use state */}
-        {props.checked === true ? (
-          <div className={checkboxIndicator}>
-            {checkEmoji ? (
-              <div className={checkboxEmoji}>{checkEmoji}</div>
-            ) : (
-              <LucideIcon name="check" className={checkboxIcon} />
-            )}
-          </div>
-        ) : (
-          checkbox.state && (
-            <div className={checkboxIndicator}>
-              {checkEmoji ? (
-                <div className={checkboxEmoji}>{checkEmoji}</div>
-              ) : (
-                <LucideIcon name="check" className={checkboxIcon} />
-              )}
-            </div>
-          )
-        )}
-      </div>
-
-      <Bento as="span" color="baseText" fontSize={fontSize}>
-        {children}
-      </Bento>
-    </Bento>
-  );
-}
-
-export default React.forwardRef(Checkbox);
-// import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
-// import React from "react";
-// import { AriaCheckboxProps, useCheckbox, VisuallyHidden } from "react-aria";
-// import { useToggleState } from "react-stately";
+// import {
+//   Checkbox as CheckboxPrimitive,
+//   CheckboxProps,
+//   useCheckboxState,
+// } from "reakit/Checkbox";
 // import { clsx } from "clsx";
 // import { Bento, LucideIcon } from "../../src";
 // import { formTone } from "../styles/theme.css";
 // import { TextProps } from "../Text";
-// import "./style.css";
 // import {
 //   checkboxEmoji,
 //   checkboxIcon,
@@ -127,89 +15,103 @@ export default React.forwardRef(Checkbox);
 //   checkboxLabel,
 //   checkboxRoot,
 // } from "./style.css";
-// export interface CheckboxProps extends CheckboxPrimitive.CheckboxProps {
-//   // basic props
-//   children?: React.ReactNode | string;
+// import { VisuallyHidden } from "react-aria";
 
-//   // map some standard props
-//   // if standard onchange event comes in
-//   onChange?: (any) => void;
-//   // extra custom  props
+// export interface CheckboxExtendedProps extends CheckboxProps {
+//   //realkit
+//   disabled?: boolean | undefined;
+//   //custom
+//   defaultChecked?: boolean | undefined;
+//   children?: React.ReactNode;
+//   tone?: keyof Colors;
+//   fontSize?:
+//     | "xs"
+//     | "sm"
+//     | "base"
+//     | "lg"
+//     | "xl"
+//     | "2xl"
+//     | "3xl"
+//     | "4xl"
+//     | "5xl"
+//     | "6xl";
 //   checkEmoji?: React.ReactNode;
-
-//   fontSize?: TextProps["fontSize"];
-
-//   tone?: TextProps["tone"];
 // }
 
-// const Checkbox = (
-//   { children, fontSize, tone = "gray", checkEmoji, ...props }: CheckboxProps,
-//   ref
-// ) => {
+// function Checkbox(
+//   {
+//     disabled,
+//     defaultChecked,
+//     children,
+//     checkEmoji,
+//     fontSize,
+//     tone = "gray",
+//     ...props
+//   }: CheckboxExtendedProps,
+//   ref: React.ForwardedRef<HTMLInputElement>
+// ) {
+//   const checkbox = useCheckboxState({
+//     state: defaultChecked,
+//   });
+//   // console.log(checkbox.state);
 //   return (
 //     <Bento
 //       fontSize={fontSize}
 //       as={"label"}
-//       className={checkboxLabel[!!props.disabled ? "disabled" : "enabled"]}
-//     >
-//       <CheckboxPrimitive.Root
-//         className={clsx(checkboxRoot, formTone[tone])}
-//         {...props}
-//         ref={ref}
-//       >
-//         <CheckboxPrimitive.Indicator className={checkboxIndicator}>
-//           {checkEmoji ? (
-//             <div className={checkboxEmoji}>{checkEmoji}</div>
-//           ) : (
-//             <LucideIcon name="check" className={checkboxIcon} />
-//           )}
-//         </CheckboxPrimitive.Indicator>
-//       </CheckboxPrimitive.Root>
-//       <span>{children}</span>
-//     </Bento>
-//   );
-// };
-
-// const _Checkbox = (
-//   {
-//     children,
-//     fontSize,
-//     tone = "gray",
-//     disabled,
-
-//     checkEmoji,
-//     ...props
-//   }: CheckboxProps,
-//   ref
-// ) => {
-//   let state = useToggleState(props);
-//   let fallbackRef = React.useRef();
-//   let domRef = ref || fallbackRef;
-
-//   let { inputProps } = useCheckbox(props, state, domRef);
-//   // console.log("inputProps", inputProps);
-//   return (
-//     <label
-//       // fontSize={fontSize}
 //       className={checkboxLabel[!!disabled ? "disabled" : "enabled"]}
 //     >
 //       <VisuallyHidden>
-//         <input ref={domRef} {...inputProps} disabled={disabled} />
+//         <CheckboxPrimitive
+//           disabled={disabled}
+//           ref={ref}
+//           {...checkbox}
+//           {...props}
+//         />
 //       </VisuallyHidden>
-
-//       <div className={clsx(checkboxRoot, formTone[tone])}>
-//         <div className={checkboxIndicator}>
-//           {state.isSelected &&
-//             (checkEmoji ? (
+//       <div
+//         data-state={
+//           props.checked !== undefined
+//             ? props.checked
+//               ? "checked"
+//               : "unchecked"
+//             : checkbox.state
+//             ? "checked"
+//             : "unchecked"
+//         } // styling based on state
+//         //@ts-ignore
+//         className={clsx(checkboxRoot, formTone[tone])}
+//       >
+//         {/* use checked if it's present, else use state */}
+//         {props.checked === true ? (
+//           <div className={checkboxIndicator}>
+//             {checkEmoji ? (
 //               <div className={checkboxEmoji}>{checkEmoji}</div>
 //             ) : (
 //               <LucideIcon name="check" className={checkboxIcon} />
-//             ))}
-//         </div>
+//             )}
+//           </div>
+//         ) : (
+//           checkbox.state && (
+//             <div className={checkboxIndicator}>
+//               {checkEmoji ? (
+//                 <div className={checkboxEmoji}>{checkEmoji}</div>
+//               ) : (
+//                 <LucideIcon name="check" className={checkboxIcon} />
+//               )}
+//             </div>
+//           )
+//         )}
 //       </div>
-//       <span>{children}</span>
-//     </label>
-//   );
-// };
 
-// export default React.forwardRef(Checkbox);
+//       <Bento as="span" color="baseText" fontSize={fontSize}>
+//         {children}
+//       </Bento>
+//     </Bento>
+//   );
+// }
+
+const Checkbox = () => {
+	return <p>Checkbox is depricated.</p>;
+};
+
+export default React.forwardRef(Checkbox);
--- src/Dialog/index.tsx
+++ src/Dialog/index.tsx
@@ -6,7 +6,7 @@ import Box from "src/Box";
 import IconButton from "src/IconButton";
 
 const Dialog = () => {
-  return "Use subcomponents to render content.";
+	return "Use subcomponents to render content.";
 };
 
 export const Root = styled(DialogPrimitive.Root, {});
@@ -19,15 +19,15 @@ export const Portal = styled(DialogPrimitive.Portal, {});
 Portal.displayName = "Portal";
 Dialog.Portal = Portal;
 export const Title = styled(DialogPrimitive.Title, {
-  fontSize: "$base",
-  color: "$baseText",
-  fontWeight: "$bold",
+	fontSize: "$base",
+	color: "$baseText",
+	fontWeight: "$bold",
 });
 Title.displayName = "Title";
 Dialog.Title = Title;
 export const Description = styled(DialogPrimitive.Description, {
-  fontSize: "$sm",
-  color: "$baseTextTertiary",
+	fontSize: "$sm",
+	color: "$baseTextTertiary",
 });
 Description.displayName = "Description";
 Dialog.Description = Description;
@@ -35,65 +35,65 @@ export const Close = styled(DialogPrimitive.Close, {});
 Close.displayName = "Close";
 Dialog.Close = Close;
 export const Overlay = styled(DialogPrimitive.Overlay, {
-  backgroundColor: blackA.blackA8,
-  position: "fixed",
-  inset: 0,
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
+	backgroundColor: blackA.blackA8,
+	position: "fixed",
+	inset: 0,
+	display: "flex",
+	alignItems: "center",
+	justifyContent: "center",
 });
 Overlay.displayName = "Overlay";
 Dialog.Overlay = Overlay;
 export const Content = styled(DialogPrimitive.Content, {
-  backgroundColor: "$baseBackground",
-  borderRadius: "$2xl",
-  boxShadow: "$2",
-  zIndex: 30,
-  position: "relative",
-  width: "90vw",
-  maxWidth: "650px",
-  // maxHeight: "85vh",
-  overflowY: "auto",
-  "&:focus": { outline: "none" },
+	backgroundColor: "$baseBackground",
+	borderRadius: "$2xl",
+	boxShadow: "$2",
+	zIndex: 30,
+	position: "relative",
+	width: "90vw",
+	maxWidth: "650px",
+	// maxHeight: "85vh",
+	overflowY: "auto",
+	"&:focus": { outline: "none" },
 });
 Content.displayName = "Content";
 Dialog.Content = Content;
 
 export const ContentWithClose = (props: any) => {
-  return (
-    <Content {...props}>
-      <Box
-        css={{
-          position: "absolute",
-          top: "$2",
-          right: "$2",
-          zIndex: "32",
-        }}
-      >
-        <DialogPrimitive.Close asChild>
-          <IconButton name="x" intent="ghost" />
-        </DialogPrimitive.Close>
-      </Box>
-      <Box
-        css={{
-          padding: "$6",
+	return (
+		<Content {...props}>
+			<Box
+				css={{
+					position: "absolute",
+					top: "$2",
+					right: "$2",
+					zIndex: "32",
+				}}
+			>
+				<DialogPrimitive.Close asChild>
+					<IconButton name="x" intent="ghost" />
+				</DialogPrimitive.Close>
+			</Box>
+			<Box
+				css={{
+					padding: "$6",
 
-          position: "relative",
-          WebkitMaskImage:
-            "linear-gradient(to bottom, rgba(0, 0, 0, 1) 95%, rgba(0, 0, 0, 0) 100%)",
-          maskImage:
-            "linear-gradient(to bottom, rgba(0, 0, 0, 1) 95%, rgba(0, 0, 0, 0) 100%)",
-          width: "100%",
-          minWidth: "100%",
-          maxHeight: "85vh",
-          overflowY: "auto",
-          ":focus": { outline: "none" },
-        }}
-      >
-        {props.children}
-      </Box>
-    </Content>
-  );
+					position: "relative",
+					WebkitMaskImage:
+						"linear-gradient(to bottom, rgba(0, 0, 0, 1) 95%, rgba(0, 0, 0, 0) 100%)",
+					maskImage:
+						"linear-gradient(to bottom, rgba(0, 0, 0, 1) 95%, rgba(0, 0, 0, 0) 100%)",
+					width: "100%",
+					minWidth: "100%",
+					maxHeight: "85vh",
+					overflowY: "auto",
+					":focus": { outline: "none" },
+				}}
+			>
+				{props.children}
+			</Box>
+		</Content>
+	);
 };
 ContentWithClose.displayName = "ContentWithClose";
 Dialog.ContentWithClose = ContentWithClose;
